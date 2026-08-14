6a19b893e8577fcc6a4a12fddf9e9bda8bd950e6
franjunoy <franjunoy@hotmail.com>
2026-08-14 16:45:21 -0300

    funciones

--- funciones.go
+++ funciones.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -10,10 +9,3 @@ func suma(a, b int) int {
 	return a + b
 }
 
-func dividir(a, b float64) (float64, error){
-	if b == 0 {
-		return 0, errors.New("No se puede dividr entre 0")
-	}
-	cociente := a / b
-	return cociente, nil
-}
